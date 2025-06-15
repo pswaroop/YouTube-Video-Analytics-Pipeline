ca64b90bb09adb1ad77bba7a46541ae000c69f89
pswaroop <sswaroop46@gmail.com>
2025-06-15 22:41:26 +0530

    Implemented Airflow DAG scheduler

---
+++ dags/youtube_data_dag.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from datetime import datetime,timedelta
+
+default_args = {
+    'owner':'swaroop',
+    'retries':1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id='youtube_trending_data_pipeline',
+    default_args=default_args,
+    start_date=(2025,6,15),
+    schedule_interval='@once', #'@daily'
+    catchup=False,
+    tags=['youtube','etl'],
+    doc_md="""
+    ### YouTube Trending ETL
+    1. Scrape trending data from YouTube API
+    2. Process it using PySpark
+    """
+) as dag:
+    run_scraper = BashOperator(
+        task_id='scrape_youtube_data',
+        bash_command='python3 scraper/youtube_scraper.py'
+    )
+
+    run_processor = BashOperator(
+        task_id='process_raw_data',
+        bash_command='python3 spark_jobs/process_data.py'
+    )
+
+    run_scraper >> run_processor
